8e73e70eb57d359dff3c7c8e60a789785274240d
paolomococci <paolomococci@gmail.com>
2021-12-08 05:32:42 +0100

    edited component PlanForm

--- plan-day/src/components/PlanForm.vue
+++ plan-day/src/components/PlanForm.vue
@@ -8,11 +8,35 @@
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import { PlanType } from "@/models/planModel";
+import { v4 as uuidv4 } from "uuid";
+
 export default defineComponent({
   name: "PlanForm",
   setup() {
     const version = ref("version 1.0");
-    return { version };
+    const newTask = ref("");
+    const task = ref<PlanType[]>([]);
+
+    function addNewTask(): void {
+      if (!newTask.value) {
+        return;
+      }
+      task.value.push({
+        id: uuidv4(),
+        done: false,
+        content: newTask.value,
+      });
+      console.log("new task: ", newTask.value);
+      newTask.value = "";
+    }
+
+    return {
+      version,
+      task,
+      newTask,
+      addNewTask
+    };
   },
 });
 </script>
