9b220b30af0c7ccee94e1bb0e3b60ed7145b8960
paolomococci <paolomococci@gmail.com>
2021-12-08 09:01:26 +0100

    I think I have finished editing component PlanForm

--- plan-day/src/components/PlanForm.vue
+++ plan-day/src/components/PlanForm.vue
@@ -1,84 +1,102 @@
 <template>
-  <div class="mb-4">
-    <h1>Plan From at work!</h1>
-    <hr />
-    <h3>TypeScript demo {{ version }}</h3>
-  </div>
-  <form @submit.prevent="addNewTask">
-    <div class="mb-5">
-      <label for="newTask" class="form-label"> New Task </label>
-      <input
-        class="form-control"
-        id="newTask"
-        placeholder="task to be scheduled"
-        v-model="newTask"
-        name="newTask"
-      />
-      <div class="mb-5 d-flex flex-row justify-content-start">
-        <div class="m-2">
-          <button type="submit" class="btn btn-primary">add a new task</button>
+  <div class="card" style="margin-top: 20px">
+    <div class="card-body">
+      <h2 class="card-title">Plan From at work!</h2>
+      <h3 class="card-subtitle mb-2 text-muted">
+        sample of web application {{ version }}
+      </h3>
+      <div>
+        <form @submit.prevent="addNewTask">
+          <div class="mb-5">
+            <label for="newTask" class="form-label">
+              Type a name for add new task:
+            </label>
+            <input
+              class="form-control"
+              id="newTask"
+              placeholder="task to be scheduled"
+              v-model="newTask"
+              name="newTask"
+            />
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+              <div class="m-2">
+                <button type="submit" class="btn btn-outline-primary btn-lg">
+                  add a new task
+                </button>
+              </div>
+              <div class="m-2">
+                <button
+                  type="button"
+                  class="btn btn-outline-danger btn-lg"
+                  @click="removeAllTask"
+                >
+                  remove all task
+                </button>
+              </div>
+              <div class="m-2">
+                <button
+                  type="button"
+                  class="btn btn-outline-success btn-lg"
+                  @click="allCheckAsDone"
+                >
+                  all check as done
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+        <div v-if="tasks.length === 0">
+          <h4>the list of tasks to be performed is empty</h4>
         </div>
-        <div class="m-2">
-          <button type="button" class="btn btn-danger" @click="removeAllTask">
-            remove all task
-          </button>
-        </div>
-        <div class="m-2">
-          <button type="button" class="btn btn-success" @click="allCheckAsDone">
-            all check as done
-          </button>
+        <div v-else>
+          <ul class="list-group">
+            <li
+              class="list-group-item d-flex flex-row justify-content-between align-items-center"
+              v-for="(task, index) in tasks"
+              :key="task.id"
+            >
+              <h4
+                :class="{ mark: task.done }"
+                style="cursor: pointer"
+                @click="checkAsDone(task)"
+              >
+                {{ task.content }}
+              </h4>
+              <div v-if="task.done">
+                <button
+                  type="button"
+                  class="btn btn-outline-warning btn-sm"
+                  @click="removeTask(index)"
+                >
+                  remove task
+                </button>
+              </div>
+              <div v-else>
+                <button
+                  type="button"
+                  class="btn btn-outline-warning btn-sm"
+                  disabled
+                  @click="removeTask(index)"
+                >
+                  remove task
+                </button>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
-  </form>
-  <div v-if="tasks.length === 0">
-    <h4>the list of tasks to be performed is empty</h4>
-  </div>
-  <div v-else>
-    <ul class="list-group">
-      <li
-        class="list-group-item d-flex flex-row justify-content-between align-items-center"
-        v-for="(task, index) in tasks"
-        :key="task.id"
-      >
-        <h4
-          :class="{ mark: task.done }"
-          style="cursor: pointer"
-          @click="checkAsDone(task)"
-        >
-          {{ task.content }}
-        </h4>
-        <button
-          type="button"
-          class="btn btn-warning"
-          @click="removeTask(index)"
-        >
-          remove task
-        </button>
-      </li>
-    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, PropType, ref } from "vue";
+import { defineComponent, ref } from "vue";
 import { PlanType } from "@/models/planModel";
 import { v4 as uuidv4 } from "uuid";
 
-type Props = {
-  title: string;
-  subTitle: string;
-};
-
 export default defineComponent({
   name: "PlanForm",
-  props: {
-    about: {
-      type: Object as PropType<Props>,
-      required: true,
-    },
-  },
-  setup(props) {
+  setup() {
     const version = ref("version 1.0");
     const newTask = ref("");
     const tasks = ref<PlanType[]>([]);
@@ -112,8 +130,6 @@ export default defineComponent({
       tasks.value.forEach((task) => (task.done = true));
     }
 
-    onMounted(() => console.log(props.about.title));
-
     return {
       version,
       tasks,
@@ -130,6 +146,6 @@ export default defineComponent({
 
 <style>
 .mark {
-  text-decoration: aquamarine;
+  text-decoration: whitesmoke line-through;
 }
 </style>
