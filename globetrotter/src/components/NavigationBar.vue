698183ed05f9ff550d966c026d384edbd7d7a6ee
paolomococci <paolomococci@gmail.com>
2021-12-16 18:19:47 +0100

    edited component NavigationBar belonging to project globetrotter

--- globetrotter/src/components/NavigationBar.vue
+++ globetrotter/src/components/NavigationBar.vue
@@ -1,7 +1,27 @@
 <template>
   <v-app-bar app color="primary" dark>
-    <div class="d-flex align-center"></div>
+    <div class="d-flex align-center">
+      <router-link to="/" class="menu">
+        <v-icon large color="blue darken-2"> mdi-message-text </v-icon>
+      </router-link>
+    </div>
     <v-spacer></v-spacer>
+    <div>
+      <v-btn color="primary" outlined :to="{ path: '/' }">
+        <span class="menu">Home</span>
+      </v-btn>
+      <router-link to="/about">
+        <v-btn color="primary" outlined>
+          <span class="menu">About</span>
+        </v-btn>
+      </router-link>
+      <v-btn color="primary" outlined :to="{ path: '/dashboard' }">
+        <span class="menu">Dashboard</span>
+      </v-btn>
+      <v-btn color="primary" outlined :to="{ path: '/login' }">
+        <span class="menu">Login</span>
+      </v-btn>
+    </div>
   </v-app-bar>
 </template>
 
