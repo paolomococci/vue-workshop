eaa235e8fa31037a64e96461b5045abd127533d6
paolomococci <paolomococci@gmail.com>
2021-12-16 17:57:32 +0100

    edited component NavigationBar belonging to project globetrotter

---
+++ globetrotter/src/components/NavigationBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center"></div>
+  <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "NavigationBar",
+};
+</script>
+
+<style scoped>
+.menu {
+  color: #fff;
+  text-decoration: none;
+}
+</style>
